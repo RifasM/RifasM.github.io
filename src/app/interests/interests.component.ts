a06a48d01abfa60a365ef1be4fbf250b3e1a96d8
Mohammed Rifas S <rifas2475@outlook.com>
2022-11-04 20:07:19 +0530

    Adding favicons and other assets

--- src/app/interests/interests.component.ts
+++ src/app/interests/interests.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-interests',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterestsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private hiddenInterestRef: ElementRef) { }
 
   ngOnInit(): void {
   }
 
+  showHiddenInterests() {
+    const ulElement = this.hiddenInterestRef.nativeElement.querySelector('ul').classList;
+    if(ulElement.contains('visually-hidden')) {
+      ulElement.remove('visually-hidden');
+    }
+    else {
+      ulElement.add('visually-hidden');
+    }
+  }
 }
